9e8d92e914df7865ef6b1927dbe1064623d4e38e
jacenfox <jacenfox@gmail.com>
2016-12-21 17:09:33 -0500

    sort activations, return index

--- DeepLearningBackend/sortActivations.py
+++ DeepLearningBackend/sortActivations.py
@@ -1,28 +1,49 @@
 from TensorflowBackend import TensorflowBackend
+# python3
 # from InputGenerators.LoaderBase import DataCaltech101Example
+# python2
+import imp
+InputGenerators = imp.load_source('LoaderBase', '../InputGenerators/LoaderBase.py')
 
 # test function
 from scipy.misc import imread, imresize
 import numpy as np
 
+# select Torch or Tensorflow as backend
 tfmodel = '/home-local/jizha16.extra.nobkp/data/ml/vgg16-tfmodel.meta'
-# loader = DataCaltech101Example()
-
 model = TensorflowBackend()
 model.load_tf_model(tfmodel)
 
 
-img = np.reshape(imresize(imread('../images/lena.png'), [224, 224]) / 255.0, [1, 224, 224, 3])
-ims = img
-model.forward(ims)
-
-
-def sortActivations(model):
-    activations = model.get_convolution_activation()
-    # [[N, H, W, C], ]
-
-    act_most = []  # [[N, C], ]
+def sortActivations(activations):
+    # activations: [[N, H, W, C], ...]
+    most_activated = []  # [[N, C], ]
     for activation in activations:
         activation = np.asarray(activation)
-        act_most.append(np.sum(activation, axis=(1, 2)))
-    return act_most
+        act_sum = np.sum(activation, axis=(0, 1, 2))  # sum or mean follow the same order
+        ind = np.argsort(act_sum)[::-1]
+        most_activated.appedn(ind)
+    return most_activated  # just return an index
+
+
+loader = InputGenerators.DataCaltech101Example()
+loader.setClass('camera')
+ims, labels = loader.next()
+model.forward(ims)
+activations = model.get_convolution_activation()
+most_activated = sortActivations(activations)
+print most_activated
+
+# do a forward pass
+loader = InputGenerators.DataCaltech101Example()
+loader.setClass('pizza', 10)
+# or only one
+# ims = np.reshape(imresize(imread('../images/lena.png'), [224, 224]) / 255.0, [1, 224, 224, 3])
+activations = []
+for i in loader.length:
+    ims, labels = loader.next()
+    model.forward(ims)
+    acts = model.get_convolution_activation()
+    activations.append(acts)
+    activations, most_activated = sortActivations(activations)
+    # [[N, H, W, C], layer2...]], [top index of C, ]
